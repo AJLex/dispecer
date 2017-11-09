b1b3ef0298177787c2c955f8c29a5f7f1128f359
AJLex <ashuliatyev@gmail.com>
2017-11-09 19:18:53 +0300

    added comments

--- proj/dispecer/views.py
+++ proj/dispecer/views.py
@@ -6,13 +6,17 @@ from .models import CurrentTrucksLoad
 
 def current_info(request):
     form = request.POST
+    # Получаем сет по всем грузовикам, везущим руду, с дополнительным полем - перегруз
     trucks = CurrentTrucksLoad.objects.annotate(
         over_load=(F('current_load')-F('trucks_info__carrying_capacity'))*100/F('trucks_info__carrying_capacity'))
+    # Получаем модели грузовиков из первоначального сета
     truck_models = trucks.values('trucks_info__truck_model').distinct()
     context = {"truck_models": truck_models}
+    # Если пользователь выбрал модель и нажал кнопку, то применяется фильтр
     if form.get('send') and form.get('truck_model') != 'all':
         context['trucks_info'] = trucks.filter(
             trucks_info__truck_model=form.get('truck_model'))
+    # иначе выводится все доступные грузовики
     else:
         context['trucks_info'] = trucks
     return render(request, 'dispecer/current_info.html', context)
